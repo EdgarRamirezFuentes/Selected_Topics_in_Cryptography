3bb03334d4703c62099a520a1db27523727acdb4
Edgar Ramírez <edgar.ramirez.fuentes.dev@gmail.com>
2023-05-14 21:04:27 -0600

    Add lab 5

---
+++ Lab5/alice.py
@@ -0,0 +1,78 @@
+import socket
+import os
+
+# ECDH Libraries
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives import hashes
+
+# Key generation
+private_key = ec.generate_private_key(ec.SECP384R1())
+
+# Sharing public key
+public_key = private_key.public_key()
+
+# Create a socket object
+s = socket.socket()
+
+# Define the port on which you want to connect
+port = 80
+
+# connect to the server on local computer
+s.connect(('localhost', port))
+
+# send data to the server
+s.send(public_key.public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo
+))
+
+# receive data from the server and decoding to get the string.
+data = s.recv(1024)
+
+# Convert PEM object to public key object
+sender_public_key = serialization.load_pem_public_key(
+    data,
+    backend=default_backend()
+)
+
+shared_key = private_key.exchange(ec.ECDH(), sender_public_key)
+
+encryption_key = HKDF(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=None,
+    info=b'handshake data',
+    backend=default_backend()
+).derive(shared_key)
+
+# Sending message encrypted with shared key using AES in GCM mode
+message = b'Hello Alice!'
+aad = b'authenticated but unencrypted data'
+aesgcm = AESGCM(encryption_key)
+nonce = os.urandom(12)
+cyphertext = aesgcm.encrypt(nonce, message, aad)
+
+# Send nonce, aad and cyphertext to Bob
+s.send(nonce)
+s.send(aad)
+s.send(cyphertext)
+
+# Receive nonce, aad and cyphertext from Bob
+nonce = s.recv(1024)
+aad = s.recv(1024)
+cyphertext = s.recv(1024)
+
+# Decrypt message
+aesgcm = AESGCM(encryption_key)
+message = aesgcm.decrypt(nonce, cyphertext, aad)
+plaintext = message.decode('utf-8')
+
+print('Bob says: ' + plaintext)
+
+# close the connection
+s.close()
+
---
+++ Lab5/bob.py
@@ -0,0 +1,86 @@
+import socket
+import os
+
+# ECDH Libraries
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives import hashes
+
+# Key generation
+private_key = ec.generate_private_key(ec.SECP384R1())
+
+# Sharing public key
+public_key = private_key.public_key()
+
+# Create a socket object
+s = socket.socket()
+
+# Define the port on which you want to connect
+port = 80
+
+# Listen for incoming connections
+s.bind(('', port))
+
+# put the socket into listening mode
+s.listen(5)
+
+# a forever loop until we interrupt it or
+# an error occurs
+while True:
+        # Establish connection with client.
+        c, addr = s.accept()
+
+        # receive data from the client and decoding to get the string.
+        data = c.recv(1024)
+    
+        # Convert PEM object to public key object
+        sender_public_key = serialization.load_pem_public_key(
+            data,
+            backend=default_backend()
+        )
+
+        # Send public key to client
+        c.send(public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        ))
+            
+        shared_key = private_key.exchange(ec.ECDH(), sender_public_key)
+
+        encryption_key = HKDF(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=None,
+            info=b'handshake data',
+            backend=default_backend()
+        ).derive(shared_key)
+    
+        # Receive nonce, aad and cyphertext from client
+        nonce = c.recv(1024)
+        aad = c.recv(1024)
+        cyphertext = c.recv(1024)
+
+        # Decrypt cyphertext using shared key using AES in GCM mode
+        aesgcm = AESGCM(encryption_key)
+        message = aesgcm.decrypt(nonce, cyphertext, aad)
+        plaintext = message.decode('utf-8')
+        
+        print("Alice says: ", plaintext)
+
+        # Reply to Alice
+        reply_message = b'Hello Alice!'
+        reply_nonce = os.urandom(12)
+        reply_aad = b'Reply authenticated but unencrypted data'
+        reply_aesgcm = AESGCM(encryption_key)
+        reply_cyphertext = reply_aesgcm.encrypt(reply_nonce, reply_message, reply_aad)
+
+        # Send nonce, aad and cyphertext to Alice
+        c.send(reply_nonce)
+        c.send(reply_aad)
+        c.send(reply_cyphertext)
+
+        # Close the connection with the client
+        c.close()
